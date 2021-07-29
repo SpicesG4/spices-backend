e35c972ddd8822ab4d956a94e02f29c011a9408e
baraahalomari <balomari995@gmail.com>
2021-07-29 11:42:53 +0300

    basic-auth

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ require('./DB/mangoose')
 require('dotenv').config()
 const  port=process.env.PORT
 
+const app=express()
+app.use(express.json())
+
 //Routes
-// const v1Routes = require('./routes/v1.js');
+const v1Routes = require('./routes/v1.js');
 
 
 //Example
-// app.use('/api/v1', v1Routes);
+app.use('/', v1Routes);
 
 
-
-
-const app=express()
-
-app.use(express.json())
 
 
 

5d5918eec56071cf71e5013cd806e03d800a7f94
Sokiyna <sokiynanaser@yahoo.com>
2021-08-03 19:08:50 +0300

    finish logout

--- routes/v1.js
+++ routes/v1.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const authRouter = express.Router();
 
+const jwt = require("jsonwebtoken");
+
 const User = require('../DB/model/user.schema');
 const basicAuth = require('../middleware/basic-auth');
 const bearerAuth = require('../middleware/bearer');
@@ -11,6 +13,8 @@ const UserController = require('../controllers/user_controller.js');
 
 
 authRouter.post('/signup', UserController.signup);
+
+
 
 authRouter.post('/signin', basicAuth, UserController.login);
 authRouter.get('/api/verify/:token', UserController.verify);
@@ -21,13 +25,29 @@ authRouter.get('/users', bearerAuth, async (req, res, next) => {
   res.status(200).json(list);
 });
 authRouter.get('/chef', bearerAuth, async (req, res, next) => {
-  const chefUsers = await User.find({role:'chef'});
+  const chefUsers = await User.find({ role: 'chef' });
   res.status(200).json(chefUsers);
 });
 
 authRouter.get('/secret', bearerAuth, async (req, res, next) => {
   res.status(200).send("Welcome to the secret area!")
 });
+
+
+
+authRouter.put("/logout", bearerAuth,UserController.logout );
+//  authRouter.post('/logout', bearerAuth, async (req, res) => {
+//   try {
+//     let randomNumberToAppend = toString(Math.floor((Math.random() * 1000) + 1));
+//     let randomIndex = Math.floor((Math.random() * 10) + 1);
+//     let hashedRandomNumberToAppend = await bcrypt.hash(randomNumberToAppend, 10);
+//     // now just concat the hashed random number to the end of the token
+//     req.token = req.token + hashedRandomNumberToAppend;
+//     return res.status(200).json('logout');
+//   } catch (err) {
+//     return res.status(500).json(err.message);
+//   }
+// });
 
 
 module.exports = authRouter;
